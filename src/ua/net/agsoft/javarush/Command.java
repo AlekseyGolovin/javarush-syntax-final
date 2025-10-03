fba956bfbbe399c7c6240f897bfa982dba9799d1
Aleksey <aleksey.golovin@ukr.net>
2025-10-03 18:33:40 +0300

    added several classes

---
+++ src/ua/net/agsoft/javarush/Command.java
@@ -0,0 +1,29 @@
+package ua.net.agsoft.javarush;
+
+public enum Command {
+
+    BAD_COMMAND("BAD_COMMAND"),
+    ENCRYPT("ENCRYPT"),
+    DECRYPT("DECRYPT"),
+    BRUTE_FORCE("BRUTE_FORCE");
+
+    private final String code;
+
+    Command(String code) {
+        this.code = code;
+    }
+
+    public static Command fromCode(String code) {
+        for (Command command : Command.values()) {
+            if (command.code.equals(code)) {
+                return command;
+            }
+        }
+        return Command.BAD_COMMAND;
+    }
+
+    public String toString() {
+        return code;
+    }
+
+}
---
+++ src/ua/net/agsoft/javarush/Util.java
@@ -0,0 +1,32 @@
+package ua.net.agsoft.javarush;
+
+import java.nio.file.Path;
+
+public class Util {
+
+    protected static boolean isFileExists(Path filepath) {
+        // TODO: Проверить что файл доступен и вернуть результат
+        // TODO: Возможно для файла реализовать отдельный класс который будет работат с файлом и заодно проверять его наличие
+
+
+
+        return true;
+    }
+
+    protected static int tryToInt(String key) {
+        // TODO: попробовать преобразовать в int. если ошибка вернуть 0 иначе результат преобразования
+
+
+        return 0;
+    }
+
+    protected static boolean isInt(String key) {
+        // TODO: попробовать преобразовать в int. если ошибка вернуть false
+
+
+
+        return true;
+    }
+
+
+}
