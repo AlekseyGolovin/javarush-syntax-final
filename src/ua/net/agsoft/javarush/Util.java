a7c2f92904796ea187bef6412ba2104e77589a09
Aleksey Golovin <108547215+AlekseyGolovin@users.noreply.github.com>
2025-10-04 22:17:36 +0300

    Update Util.java
    added util tryToInt

--- src/ua/net/agsoft/javarush/Util.java
+++ src/ua/net/agsoft/javarush/Util.java
@@ -7,25 +7,25 @@ public class Util {
     protected static boolean isFileExists(Path filepath) {
         // TODO: Проверить что файл доступен и вернуть результат
         // TODO: Возможно для файла реализовать отдельный класс который будет работат с файлом и заодно проверять его наличие
-
-
-
-        return true;
+        return Files.isRegularFile(filepath);
     }
 
     protected static int tryToInt(String key) {
         // TODO: попробовать преобразовать в int. если ошибка вернуть 0 иначе результат преобразования
-
-
-        return 0;
+        try {
+            return Integer.parseInt(text.trim());
+        } catch (NumberFormatException ex) {
+            return 0;
+        }
     }
 
     protected static boolean isInt(String key) {
         // TODO: попробовать преобразовать в int. если ошибка вернуть false
-
-
-
-        return true;
+        try {
+            return true
+        } catch (NumberFormatException ex) {
+            return false;
+        }
     }
 
 
